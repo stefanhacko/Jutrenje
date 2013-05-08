68e7cabc5c4d8dd744f636c3ced56850de443a98
Stefan Hacko <stefanhacko@Stefans-iMac.local>
2013-05-09 00:40:37 +0200

    Izmene u storyboardu

--- JutarnjiStihov/JutarnjiStihov/JutarnjiStihoviViewController.h
+++ JutarnjiStihov/JutarnjiStihov/JutarnjiStihoviViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface JutarnjiStihoviViewController : UIViewController
+
 @property (weak, nonatomic) IBOutlet UITextView *tekst;
+@property (weak, nonatomic) IBOutlet UINavigationBar *navBar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *datum;
 
 -(void)zoom:(UIPinchGestureRecognizer *)rec;
 
